4a8523ebbae31e35a4d256ff38faa522852cda38
Charles <cjmrkvicka@gmail.com>
2025-08-12 15:21:47 -0500

    doing more testing and adding venv and requirements.txt

--- game.py
+++ game.py
@@ -1,5 +1,17 @@
-# import pandas as pd
+print('----------STARTING NEW RUN----------')
+
+import pandas as pd
+import random
+
+# for i in range(5):
+#     print('Hello World')
+
+lowest_num = 0
+highest_num = 100
+
+random_number = random.randint(lowest_num, highest_num)
+print('PRINTING RANDOM NUMBER BETWEEN: ' , str(lowest_num) , ' AND ' , str(highest_num))
+print(random_number)
 
 
-for i in range(5):
-    print('Hello World')
+print('----------FINISHED RUN----------')
